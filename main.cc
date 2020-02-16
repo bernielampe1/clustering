cd69366d9848dcb21d7b4b0a4729929f3a4cd06d
Your Name <you@example.com>
2020-02-16 13:15:07 -0500

    added Image implementation

--- main.cc
+++ main.cc
@@ -1,13 +1,12 @@
 #include "cluster.h"
 
-int main(int argc, char **argv)
-{
-    // proc args
-    // input image
-    // create vectors
-    // cluster vectors
-    // render vectors into new image
-    // write output color image with clusters
+int main(int argc, char **argv) {
+  // proc args
+  // input image
+  // create vectors
+  // cluster vectors
+  // render vectors into new image
+  // write output color image with clusters
 
-    return 0;
+  return 0;
 }
