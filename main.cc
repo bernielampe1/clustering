9ad6cd9c35e1ba1d8c02fc149e0dc074989686a1
Your Name <you@example.com>
2020-02-19 09:46:14 -0500

    outlined main

--- main.cc
+++ main.cc
@@ -1,25 +1,38 @@
 #include "cluster.h"
 
 int main(int argc, char **argv) {
-  Vec<float> v;
-  Matrix<float> m;
-  Image<u8> im;
-
-  // proc args
-  // input image
-  // create vectors as Matrix
-  // cluster vectors creating Matrix and labels using algorithms name from args
-  // render vectors into new image
-  // write output color image with clusters
 
   u8 nclusters;
+  Image<RGB_t> imRGB;
+  Image<u8> imGray;
   Vec<u8> labels;
   Matrix<float> pts;
   Matrix<float> clusters;
+  string fname, algo, ext;
 
+  fname = argv[1];
+  algo = argv[2];
+
+  ext = fname.substr(fname.find_last_of(".") + 1);
+  if (ext == "ppm") {
+    imRGB.readFromFile(argv[1]);
+  }
+  else if (ext == "pgm") {
+    imGray.readFromFile(argv[1]);
+  }
+  else {
+    throw Exception("unknown file type, only accepts pgm and ppm");
+  }
+
+  // create pts from image
+
+  // cluster vectors creating Matrix and labels using algorithms name from args
   if (cluster("kmeans", pts, labels, nclusters, clusters)) {
     throw Exception("kmeans cluster failed");
   }
+
+  // render vectors into new image
+  // write output color image with clusters
 
   return 0;
 }
