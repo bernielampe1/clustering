1ec9cdc11debf6288e4967d1400198081827e796
Your Name <you@example.com>
2020-02-19 17:11:50 -0500

    init report and refactor repos

--- data/run.sh
+++ data/run.sh
@@ -2,12 +2,12 @@
 
 for i in kmeans
 do
-    ../cluster cluster0.pts $i 3
-    ../cluster cluster1.pts $i 7
-    ../cluster cluster2.pts $i 2
-    ../cluster cluster3.pts $i 2
-    ../cluster cluster4.pts $i 3
-    ../cluster cluster5.pts $i 3
-    ../cluster ./us.ppm $i 10
+    ../src/cluster cluster0.pts $i 3
+    ../src/cluster cluster1.pts $i 7
+    ../src/cluster cluster2.pts $i 2
+    ../src/cluster cluster3.pts $i 2
+    ../src/cluster cluster4.pts $i 3
+    ../src/cluster cluster5.pts $i 3
+    ../src/cluster ./us.ppm $i 10
 done
 
