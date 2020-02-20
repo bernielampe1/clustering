33ba8b930bc6b7a3ef27d307c387c9ef78404a1b
Your Name <you@example.com>
2020-02-19 21:20:37 -0500

    added concentric circles data set

--- data/run.sh
+++ data/run.sh
@@ -8,6 +8,7 @@ do
     ../src/cluster cluster3.pts $i 2
     ../src/cluster cluster4.pts $i 3
     ../src/cluster cluster5.pts $i 3
+    ../src/cluster cluster6.pts $i 2
     ../src/cluster ./us.ppm $i 10
 done
 
